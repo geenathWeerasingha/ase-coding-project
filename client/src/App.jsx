b4637060563c96a832d9b092f95c0dafe85250fc
geenathWeerasingha <73776728+geenathWeerasingha@users.noreply.github.com>
2024-08-01 13:24:22 +0530

    modify App.css and App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ROLES = {
+  'User': 2001 
+}
 
+
+function App() {
+   
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      {/* public routes */}
+      
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="unauthorized" element={<Unauthorized />} />
+
+
+
+      {/* we want to protect these routes */}
+      <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Route>
+
+    
+     
+    </Route>
+    
+     {/* catch all */}
+     <Route path="*" element={<Missing />} />
+  </Routes>
   )
 }
 
